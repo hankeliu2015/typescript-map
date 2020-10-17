c64f5e1e35363bee0ed993feb82e7666d00d0430
Hank Liu <hanke.liu@gmail.com>
2020-10-16 21:25:59 -0400

    testing google docs syntax on marker

---
+++ src/customMap.ts
@@ -0,0 +1,60 @@
+// import {User} from './User';
+// import {Company} from './Company';
+
+interface Mappable {
+  location: {
+    lat: number,
+    lng: number
+  };
+  markerContent(): string;
+}
+
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(divId), {
+        zoom: 1,
+        center: {
+        lat: 0,
+        lng: 0
+      }
+    });
+  }
+
+  addMarker(mappable: Mappable): void {
+    const marker = new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng
+      }
+    })
+
+    marker.addListener('click', () => {
+      const InfoWindow = new google.maps.InfoWindow(
+        {content: `${mappable.markerContent()}`});
+      InfoWindow.open(this.googleMap, marker);
+    })
+  }
+
+  // addMarker(user: User | Company): void {
+  //   new google.maps.Marker({
+  //     map: this.googleMap,
+  //     position: {
+  //       lat: user.location.lat,
+  //       lng: user.location.lng
+  //     }
+  //   })
+  // }
+
+  // addCompanyMarker(company: Company): void {
+  //   new google.maps.Marker({
+  //     map: this.googleMap,
+  //     position: {
+  //       lat: company.location.lat,
+  //       lng: company.location.lng
+  //     }
+  //   })
+  // }
+} //end of class
